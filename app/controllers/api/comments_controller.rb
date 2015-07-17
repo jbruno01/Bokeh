11c24c0d9654cf84a028f41dc0c4d91884df1412
John Bruno <jbruno01@gmail.com>
2015-07-17 14:48:40 -0400

    Comments Controller and resource in router.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,38 @@
 module Api
   class CommentsController < ApiController
-    
+
+    def new
+      @comment = Comment.new
+    end
+
+    def create
+      @comment = Comment.create(comment_params)
+      if @comment.save
+        render :show
+      else
+        render json: @comment.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def show
+      @comment = Comment.find(params[:id])
+      if @comment
+        render :show
+      else
+        render :json @comment.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def index
+      @comments = Comment.where(photo_id: params[:id])
+      render :show
+    end
+
+    private
+
+    def comment_params
+      params.require(:comment).permit(:user_id, :photo_id, :content)
+    end
+
   end
 end
