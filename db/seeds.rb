4885c056dbab66bbdeb546fba7cfe257743146e8
John Bruno <jbruno01@gmail.com>
2015-07-28 07:28:47 -0400

    Remove seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 
 
-  tony = User.create( name: "Tony", email: "tony@stark.inc", password: "ironman")
-  nick = User.create(name: "Nick" , email: "fury@shield.gov", password: "nickfury")
-  hulk = User.create(name: "HULK", email: "green@smash.ow", password: "imangry")
-
-  a1 = Album.create(title: "Tech", description: "Cool Stuff!", user_id: tony.id)
-  a2 = Album.create(title: "Suits", description: "Top Secret", user_id: tony.id)
-  a3 = Album.create(title: "Soothing", description: "Quell the Anger", user_id: hulk.id)
-  a4 = Album.create(title: "Villians", description: "Shields next targets", user_id: nick.id)
-
-  
+  # tony = User.create( name: "Tony", email: "tony@stark.inc", password: "ironman")
+  # nick = User.create(name: "Nick" , email: "fury@shield.gov", password: "nickfury")
+  # hulk = User.create(name: "HULK", email: "green@smash.ow", password: "imangry")
+  #
+  # a1 = Album.create(title: "Tech", description: "Cool Stuff!", user_id: tony.id)
+  # a2 = Album.create(title: "Suits", description: "Top Secret", user_id: tony.id)
+  # a3 = Album.create(title: "Soothing", description: "Quell the Anger", user_id: hulk.id)
+  # a4 = Album.create(title: "Villians", description: "Shields next targets", user_id: nick.id)
+  #
+  #
