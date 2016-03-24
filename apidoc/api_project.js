5a598b2b2ac18724631c9d12c2470dce73164583
John Bruno <jbruno01@gmail.com>
2016-03-24 17:23:50 -0400

    Add Solano yml.

---
+++ apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Bokeh",  "version": "1.0",  "description": "A photo sharing site",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-01-12T20:35:12.760Z",    "url": "http://apidocjs.com",    "version": "0.14.0"  }});
